d95243871506c8810d4a5c10d341a36e3f31fae0
Priscilha <priscilha@gmail.com>
2023-05-16 18:00:30 -0300

    shake

---
+++ script.js
@@ -0,0 +1,20 @@
+const screen1 = document.querySelector(".screen1")
+const screen2 = document.querySelector(".screen2")
+
+const phrase = [
+    "A vida trará coisas boas se tiver paciência.",
+    "Demonstre amor e alegria em todas as oportunidades e verá que a paz nasce dentro de si.",
+    "Não compense na ira o que lhe falta na razão.",
+    "Defeitos e virtudes são apenas dois lados da mesma moeda.",
+    "A maior de todas as torres começa no solo.",
+    "Não há que ser forte. Há que ser flexível.",
+    "Todos os dias organiza os seus cabelos, por que não faz o mesmo com o coração?",
+    "Há três coisas que jamais voltam; a flecha lançada, a palavra dita e a oportunidade perdida.",
+    "A juventude não é uma época da vida, é um estado de espírito.",
+    "Podemos escolher o que semear, mas somos obrigados a colher o que plantamos.",
+    "Dê toda a atenção á formação dos seus filhos, sobretudo com bons exemplos da sua própria vida.",
+    "Siga os bons e aprenda com eles.",
+    "Não importa o tamanho da montanha, ela não pode tapar o sol.",
+    "O bom-senso vale mais do que muito conhecimento.",
+    "Quem quer colher rosas tem de estar preparado para suportar os espinhos."
+]
